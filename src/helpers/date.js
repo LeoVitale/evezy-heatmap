fbfbdb877aaeb7fafe1c66a4ed32fff2af55a528
Leonardo Vitale <leonardo.vitale@outlook.com>
2019-10-28 02:16:03 +0100

    added date functions

---
+++ src/helpers/date.js
@@ -0,0 +1,23 @@
+export const getFormatedDate = dt => {
+  let day = dt.getDate();
+  let month = dt.getMonth() + 1;
+
+  if (day < 10) {
+    day = `0${day}`;
+  }
+  if (month < 10) {
+    month = `0${month}`;
+  }
+  return `${dt.getFullYear()}-${month}-${day}`;
+};
+
+export const setDateRange = (startDate, endDate) => {
+  const dates = [];
+  const dt = new Date(startDate);
+  while (dt <= endDate) {
+    dates.push(getFormatedDate(dt));
+    dt.setDate(dt.getDate() + 1);
+  }
+
+  return dates;
+};
